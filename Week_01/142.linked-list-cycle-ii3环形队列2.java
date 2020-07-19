d3bc05a2ecf2d56c45b64d0b624db30b7770e608
xuxiaojin <1319039722@qq.com>
2020-07-19 20:02:36 +0800

    环形队列第三遍

---
+++ Week_01/142.linked-list-cycle-ii3环形队列2.java
@@ -0,0 +1,37 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ * @lc app=leetcode id=142 lang=java
+ *
+ * [142] Linked List Cycle II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        Set<ListNode> set=new HashSet<>();
+
+        while(head!=null){
+            if(set.contains(head)){
+                return head;
+            }
+            set.add(head);
+            head=head.next;
+        }
+        return null;
+    }
+}
+// @lc code=end
+
