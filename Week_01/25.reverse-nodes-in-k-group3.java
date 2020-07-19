4257229db02b3049e0f1bdb385066e73bfb94393
xuxiaojin <1319039722@qq.com>
2020-07-19 22:03:06 +0800

    翻转k个链表节点第三遍

---
+++ Week_01/25.reverse-nodes-in-k-group3.java
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=25 lang=java
+ *
+ * [25] Reverse Nodes in k-Group
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        
+        if(head==null||head.next==null){
+            return head;
+        }
+
+        ListNode tail=head;
+        for(int i=0;i<k;i++){
+            if(tail==null){
+                return head;
+            }
+            tail=tail.next;
+        }
+
+        ListNode newHead=reverse(head,tail);
+        head.next=reverseKGroup(tail, k);
+    }
+
+    public ListNode reverse(ListNode head,ListNode tail){
+        ListNode pre=null;
+        ListNode next=null;
+
+        while(head!=tail){
+            next=head.next;
+            head.next=pre;
+            pre=head;
+            head=next;
+        }
+        return pre;
+    }
+}
+// @lc code=end
+
