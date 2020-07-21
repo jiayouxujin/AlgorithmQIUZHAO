8c7b7b90fe2317071702757e8949486155050540
xuxiaojin <1319039722@qq.com>
2020-07-21 08:22:35 +0800

    中序排列第一二遍

---
+++ Week_02/day02/94.binary-tree-inorder-traversal中序排序递归.java
@@ -0,0 +1,46 @@
+import java.util.List;
+
+/*
+ * @lc app=leetcode id=94 lang=java
+ *
+ * [94] Binary Tree Inorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res=new ArrayList<>();
+        helper(root,res);
+        return res;
+    }
+
+    public void helper(TreeNode root,List<Integer> res){
+        if(root==null){
+            return ;
+        }
+        if(root.left!=null){
+            helper(root.left, res);
+        }
+        res.add(root.val);
+        if(root.right!=null){
+            helper(root.right, res);
+        }
+    }
+}
+// @lc code=end
+
