52ca3f96b1f728f5fd0354ce6cf4c96d5cb731e0
xuxiaojin <1319039722@qq.com>
2020-07-23 14:16:34 +0800

    第一二遍

---
+++ Week_02/day04/111.minimum-depth-of-binary-tree.java
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=111 lang=java
+ *
+ * [111] Minimum Depth of Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root==null) return 0;
+        Queue<TreeNode> queue=new LinkedList<>();
+        int level=1;
+        queue.add(root);
+
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            while(size-->0){
+                TreeNode cur=queue.poll();
+                if(cur.left==null&&cur.right==null){
+                    return level;
+                }
+
+                if(cur.left!=null){
+                    queue.add(cur.left);
+                }
+
+                if(cur.right!=null){
+                    queue.add(cur.right);
+                }
+            }
+
+            level++;
+        }
+
+        return level;
+    }
+}
+// @lc code=end
+
---
+++ Week_02/day04/111.minimum-depth-of-binary-tree1.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=111 lang=java
+ *
+ * [111] Minimum Depth of Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root==null) return 0;
+        int left=minDepth(root.left);
+        int right=minDepth(root.right);
+
+        return (left==0||right==0)?left+1+right:1+Math.min(left,right);
+    }
+}
+// @lc code=end
+
