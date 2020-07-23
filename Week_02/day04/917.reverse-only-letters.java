42d9142cb0e98e1aff91c824dc5591c922ce35dc
xuxiaojin <1319039722@qq.com>
2020-07-23 22:28:39 +0800

    双指针

---
+++ Week_02/day04/917.reverse-only-letters.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=917 lang=java
+ *
+ * [917] Reverse Only Letters
+ */
+
+// @lc code=start
+class Solution {
+    public String reverseOnlyLetters(String S) {
+        StringBuilder sb=new StringBuilder(S);
+
+        int left=0,right=S.length()-1;
+        while(left<right){
+            if(!Character.isLetter(sb.charAt(left))){
+                left++;
+            }else if(!Character.isLetter(sb.charAt(right))){
+                right--;
+            }else{
+                sb.setCharAt(left,S.charAt(right));
+                sb.setCharAt(right--,S.charAt(left++));
+            }
+        }
+        return sb.toString();
+    }
+}
+// @lc code=end
+
