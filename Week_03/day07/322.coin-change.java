2713e474d24a5504dc6b341a0f4b8c37dcc7ea30
xuxiaojin <1319039722@qq.com>
2020-08-02 15:07:47 +0800

    刷旧题

---
+++ Week_03/day07/322.coin-change.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=322 lang=java
+ *
+ * [322] Coin Change
+ */
+
+// @lc code=start
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if(amount<0){
+            return -1;
+        }
+
+        return cal(coins,new int[amount],amount);
+    }
+
+    public int cal(int[] coins,int[] dp,int rem){
+        if(rem<0){
+            return -1;
+        }
+        if(rem==0){
+            return 0;
+        }
+        if(dp[rem-1]!=0){
+            return dp[rem-1];
+        }
+
+        int min=Integer.MAX_VALUE;
+        for(int coin:coins){
+            int sub=cal(coins,dp,rem-coin);
+            if(sub>=0&&sub+1<min){
+                min=sub+1;
+            }
+        }
+
+        dp[rem-1]=min==Integer.MAX_VALUE?-1:min;
+        return dp[rem-1];
+    }
+}
+// @lc code=end
+
