6b94767dc6b055661694294b54ca1a4a86590948
xuxiaojin <1319039722@qq.com>
2020-09-07 16:19:38 +0800

    动态规划

---
+++ Week_10/day01/5.longest-palindromic-substring.java
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=5 lang=java
+ *
+ * [5] Longest Palindromic Substring
+ */
+
+// @lc code=start
+class Solution {
+    public String longestPalindrome(String s) {
+        int len=s.length();
+        if(len<2){
+            return s;
+        }
+
+        boolean[][] dp=new boolean[s.length()][s.length()];
+        int begin=0,maxlen=1;
+        for(int i=0;i<s.length();i++){
+            dp[i][i]=true;
+        }
+        for(int j=1;j<s.length();j++){
+            for(int i=0;i<j;i++){
+                if(s.charAt(i)!=s.charAt(j)){
+                    dp[i][j]=false;
+                }else{
+                    if(j-i<3){
+                        dp[i][j]=true;
+                    }else{
+                        dp[i][j]=dp[i+1][j-1];
+                    }
+                }
+
+                if(dp[i][j]&&j-i+1>maxlen){
+                    begin=i;
+                    maxlen=j-i+1;
+                }
+            }
+        }
+        return s.substring(begin,begin+maxlen);
+    }
+}
+// @lc code=end
+
---
+++ Week_10/day01/5.longest-palindromic-substring_1.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=5 lang=java
+ *
+ * [5] Longest Palindromic Substring
+ */
+
+// @lc code=start
+class Solution {
+    public String longestPalindrome(String s) {
+        if(s==null||s.length()==0){
+            return "";
+        }
+        boolean[][] dp=new boolean[s.length()][s.length()];
+        int begin=0,maxlen=1;
+        for (int i = 0; i < s.length(); i++) {
+            dp[i][i] = true;
+        }
+
+        for(int j=0;j<s.length();j++){
+            for(int i=0;i<j;i++){
+                if(s.charAt(i)!=s.charAt(j)){
+                    dp[i][j]=false;
+                }else{
+                    if(j-i<3){
+                        dp[i][j]=true;
+                    }else{
+                        dp[i][j]=dp[i+1][j-1];
+                    }
+                }
+                if(dp[i][j]&&j-i+1>maxlen){
+                    begin=i;
+                    maxlen=j-i+1;
+                }
+            }
+        }
+        return s.substring(begin,begin+maxlen);
+    }
+}
+// @lc code=end
+
