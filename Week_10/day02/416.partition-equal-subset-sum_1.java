2533175267466180eca7e4bdf82a09e360287c9f
xuxiaojin <1319039722@qq.com>
2020-09-08 09:38:37 +0800

    新题

---
+++ Week_10/day02/416.partition-equal-subset-sum_1.java
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=416 lang=java
+ *
+ * [416] Partition Equal Subset Sum
+ */
+
+// @lc code=start
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int sum=0;
+        
+        for(int n:nums){
+            sum+=n;
+        }
+        if(sum%2!=0){
+            return false;
+        }
+        int weight=sum/2;
+        boolean[][] dp=new boolean[nums.length+1][weight+1];
+        
+        for(int i=0;i<=nums.length;i++){
+            dp[i][0]=true;
+        }
+        
+        for(int i=1;i<=nums.length;i++){
+            for(int j=1;j<=weight;j++){
+                if(nums[i-1]<=j){
+                    dp[i][j]=dp[i-1][j]|dp[i-1][j-nums[i-1]];
+                }else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[nums.length][weight];
+    }
+}
+// @lc code=end
+
